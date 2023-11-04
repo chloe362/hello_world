65925957c48edca1a4b2300043c04a9549174479
Chloe <chloelam362@gmail.com>
2023-11-04 12:05:00 +0800

    Add Calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
- 
+function calc() {
+    const inputA = document.querySelector("#a");
+    const inputB = document.querySelector("#b");
+    const operator = document.querySelector("#operator");
+    const result = document.querySelector("#result");
+  
+    const a = inputA.value *1;
+    const b = inputB.value*1;
+    const op = operator.value;
+  
+    if (op === "+") {
+      result.innerText = a + b;
+    }
+  
+    if (op === "-") {
+      result.innerText = a - b;
+    }
+  
+    if (op === "*") {
+      result.innerText = a * b;
+    }
+  
+    if (op === "/") {
+      result.innerText = a / b;
+    }
+  }
+  
+  const btn = document.querySelector("button");
+  // btn.onclick = calc;
+  btn.addEventListener("click", calc);
