7032af198618169e8232b07891171ef7c662bd43
Chloe <chloelam362@gmail.com>
2023-11-04 12:47:31 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,28 +3,28 @@ function calc() {
     const inputB = document.querySelector("#b");
     const operator = document.querySelector("#operator");
     const result = document.querySelector("#result");
-  
-    const a = inputA.value *1;
-    const b = inputB.value*1;
+
+    const a = inputA.value * 1;
+    const b = inputB.value * 1;
     const op = operator.value;
-  
+
     if (op === "+") {
-      result.innerText = a + b;
+        result.innerText = a + b;
     }
-  
+
     if (op === "-") {
-      result.innerText = a - b;
+        result.innerText = a - b;
     }
-  
+
     if (op === "*") {
-      result.innerText = a * b;
+        result.innerText = a * b;
     }
-  
+
     if (op === "/") {
-      result.innerText = a / b;
+        result.innerText = a / b;
     }
-  }
-  
-  const btn = document.querySelector("button");
-  // btn.onclick = calc;
-  btn.addEventListener("click", calc);
+}
+
+const btn = document.querySelector("button");
+// btn.onclick = calc;
+btn.addEventListener("click", calc);
