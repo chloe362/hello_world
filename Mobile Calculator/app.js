38cebd0dae07e472d8b881b5db6e8ec8c40859b1
Chloe <chloelam362@gmail.com>
2023-11-11 12:53:25 +0800

    equal

--- Mobile Calculator/app.js
+++ Mobile Calculator/app.js
@@ -1,7 +1,11 @@
 const screen = document.querySelector('#screen')
 const numBtns = document.querySelectorAll("button.num");
+const oprBtns = document.querySelectorAll("button.opr");
+const calcBtn = document.querySelector("button.calculate")
+let a = 0
+let operation = null
+let b = 0
 
-let a = 0;
 for (let i = 0; i < numBtns.length; i++) {
     const numBtn = numBtns[i];
 
@@ -9,7 +13,21 @@ for (let i = 0; i < numBtns.length; i++) {
         alert(numBtn.dataset.num)
     }
     numBtn.addEventListener("click", function () {
-        a = a + parseInt(numBtn.dataset.num,10 );
-        screen.innerHTML = a;
+        if (operation !== null){
+        b = b + parseInt(numBtn.dataset.num,10 );
+        screen.innerHTML = +b;
+    }
+    a = a + parseInt(numBtn.dataset.num,10 );
+    screen.innerHTML = +a;
     })
  }  
+
+ for(let i = 0; i < oprBtns.length; i++){
+    const oprBtn = oprBtns[i]
+    oprBtn.addEventListener("click", function(){
+        operation = oprBtn.dataset.opr
+ })
+ }
+ calcBtn.addEventListener("click", function(){
+    
+ })
